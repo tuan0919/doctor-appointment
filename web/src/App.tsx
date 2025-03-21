ca6db20fc02f13968a4b1abf20dd5f55d8ec4423
Quynhlieu <21130509@st.hcmuaf.edu.vn>
2025-03-21 10:17:19 +0700

    doctor item card ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,9 +4,23 @@ import DoctorItemCard from "./components/DoctorItemCard.tsx";
 function App() {
 
   return (
-    <>
-        <DoctorItemCard/>
-    </>
+      <div className="p-4">
+          {/* Hiển thị danh sách bác sĩ theo dạng lưới 4 cột */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+              <DoctorItemCard/>
+          </div>
+      </div>
   )
 }
 
