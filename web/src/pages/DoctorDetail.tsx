55892bb959eaf6458d22b2d3f963ac59de1fd7df
Chuc-ngan <ngannyn16122003@gmail.com>
2025-03-21 13:28:22 +0700

    doctor detail

---
+++ web/src/pages/DoctorDetail.tsx
@@ -0,0 +1,13 @@
+function DoctorDetail() {
+    return (
+        <div className="max-w">
+
+            <main className="w-full">
+                <h1 className="text-center text-2xl font-bold my-4">Chào mừng đến với Medpro</h1>
+            </main>
+
+        </div>
+    );
+}
+
+export default DoctorDetail;
